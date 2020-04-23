ad840a1662301bbccbff07f206636f7a82d9e657
Max <mdunaevs@andrew.cmu.edu>
2020-04-23 00:01:31 -0400

    Added more funtionality to the gun and zombie enemy

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public float enemyHealth = 100.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyHealth <= 0){
+            EnemyDead();
+        }
+    }
+
+    public void DeductHealth(float damageTaken){
+        enemyHealth -= damageTaken;
+    }
+
+    public void EnemyDead(){
+        Destroy(this.gameObject);
+    }
+}
